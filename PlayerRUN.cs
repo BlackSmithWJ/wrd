93fa4ec95133a04c57cd5b3e02aa9bd001b46baf
BlackSmithWJ <44721514+BlackSmithWJ@users.noreply.github.com>
2018-11-03 16:56:28 +0900

    Add files via upload
    DAY1

---
+++ PlayerRUN.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRUN : PlayerFSMState
+{
+
+    public Transform marker;
+    public float moveSpeed = 3.0f;
+    void Start()
+    {
+        marker = GameObject.FindGameObjectWithTag("Marker").transform;
+    }
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+        transform.position,
+        marker.position,
+        moveSpeed * Time.deltaTime);
+    }
+}
+
+
